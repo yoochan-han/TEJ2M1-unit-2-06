44fcfae761b43dae5cbf07e99e456d00350e92a5
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2025-03-25 09:28:05 -0400

    Create arduino.c
    rangefinder program in c

---
+++ arduino.c
@@ -0,0 +1,31 @@
+/*
+  created on Mar 2025
+  by yoochan Han
+  This program print distance in seria with sonar
+*/
+
+const int trigPin = 9;
+const int echoPin = 10;
+
+float duration, distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  Serial.print("Distance : ");
+  Serial.println(distance);
+  Serial.print("Distance");
+  delay(100);
+}
